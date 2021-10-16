8a8ef13342dac3f734257cead17cd5ad982761f3
Elena María Pérez Pérez <elena.perez.perez@alumnos.upm.es>
2021-10-16 13:54:05 +0200

    new article button

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../interfaces/article';
-import {Alert} from '../interfaces/alert'
+import { Alert } from '../interfaces/alert'
 import { LoginService } from '../services/login.service';
 import { NewsService } from '../services/news.service';
 
@@ -53,32 +53,28 @@ export class ArticleListComponent implements OnInit {
     this.selectedCategory = category;
   }
 
-
     
-    deleteArticle(article:Article) {
-      if(confirm("Do you want to delete this article: " + article.title + " ?" )) {
-        this.newsService.deleteArticle(article).subscribe(article => { 
+  deleteArticle(article:Article) {
+    if(confirm("Do you want to delete this article: " + article.title + " ?" )) {
+      this.newsService.deleteArticle(article).subscribe(article => { 
+          this.alerts.push({
+            type: 'success',
+            alertMessage: 'Article deleted',
+          });
+          this.getServerArticles(); },
+          error => {
             this.alerts.push({
-              type: 'success',
-              alertMessage: 'Article deleted',
+              type: 'danger',
+              alertMessage: 'The article has not been deleted',
             });
-            this.getServerArticles(); },
-            error => {
-              this.alerts.push({
-                type: 'danger',
-                alertMessage: 'The article has not been deleted',
-              });
-              console.log('error in deletion of article');
-              console.log(error);
-              this.getServerArticles();
-            },()=>{
-              console.log('The article has been deleted');
-            }
-        );
-      }
+            console.log('error in deletion of article');
+            console.log(error);
+            this.getServerArticles();
+          },()=>{
+            console.log('The article has been deleted');
+          }
+      );
     }
-  
-
-
+  }
 
 }
