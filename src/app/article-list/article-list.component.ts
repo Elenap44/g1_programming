309cb5ae671ba795255448e67ffbf72a37042f5b
Elena María Pérez Pérez <elena.perez.perez@alumnos.upm.es>
2021-10-16 14:30:41 +0200

    errors

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../interfaces/article';
-import { Alert } from '../interfaces/alert'
+import { Alerts } from '../interfaces/alerts'
 import { LoginService } from '../services/login.service';
 import { NewsService } from '../services/news.service';
 
@@ -15,7 +15,7 @@ export class ArticleListComponent implements OnInit {
   selectedCategory!: string;
   articleList!: Article[];
   article!: Article;
-  alerts!: Alert[];
+  alerts!: Alerts[];
 
   constructor(public loginService: LoginService, public newsService: NewsService) { 
     this.selectedCategory= 'All';
@@ -58,20 +58,15 @@ export class ArticleListComponent implements OnInit {
     if(confirm("Do you want to delete this article: " + article.title + " ?" )) {
       this.newsService.deleteArticle(article).subscribe(article => { 
           this.alerts.push({
-            type: 'success',
-            alertMessage: 'Article deleted',
+            message: 'The article has been deleted',
           });
           this.getServerArticles(); },
           error => {
             this.alerts.push({
-              type: 'danger',
-              alertMessage: 'The article has not been deleted',
+              message: 'Error, the article has not been deleted',
             });
-            console.log('error in deletion of article');
-            console.log(error);
             this.getServerArticles();
           },()=>{
-            console.log('The article has been deleted');
           }
       );
     }
